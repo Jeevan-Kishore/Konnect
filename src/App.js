0ab24bb56bdd31f66a3bb53522b25392f9ae22d2
jeevan-kishore <kishor080@gmail.com>
2020-05-24 01:32:14 +0530

    feat(init): Init site :zap:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./media-query.css";
+import styles from "./app.module.css";
+import mySvg from "./svg/tree.svg";
+import { MenuBar } from "./components/Menu-Bar";
+import { Card } from "./components/Card";
 
 function App() {
-  return <div>Here</div>;
+  return (
+    <>
+      <MenuBar />
+      <div
+        className={styles.wrapper}
+        style={{ backgroundImage: `url(${mySvg})` }}
+      >
+        <div className={styles.container}>
+          <Card />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Menu-Bar/index.js
+++ src/components/Menu-Bar/index.js
@@ -1,1 +1,18 @@
 import React from "react";
+import styles from "./menu-bar.module.css";
+import { KonnectLogo } from "../Logo";
+
+export const MenuBar = (props) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.logoContainer}>
+        <KonnectLogo />
+      </div>
+      <ul className={styles.listItemsContainer}>
+        <li>first</li>
+        <li>sec</li>
+        <li>third</li>
+      </ul>
+    </div>
+  );
+};
